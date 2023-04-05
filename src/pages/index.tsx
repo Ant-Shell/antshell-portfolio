ac6bc8d7f4c9ff2432464c8c3ee0cb120d2c8bf3
Anthony Shellman <atshellman@gmail.com>
2023-04-04 18:18:19 -0600

    Import and add Hero component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
 import Head from 'next/head'
 import Header from '../components/header'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Hero from '../components/hero'
 
 export default function Home() {
   return (
@@ -15,6 +13,7 @@ export default function Home() {
       </Head>
       <main>
         <Header />
+        <Hero />
       </main>
     </>
   )
