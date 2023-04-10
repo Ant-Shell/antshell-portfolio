bbfb54cd4a27e578c60ec19b0bc5704d057a3ae7
Anthony Shellman <atshellman@gmail.com>
2023-04-10 17:37:07 -0600

    Add palaceholders for skill icons and temporary styling

---
+++ src/components/skills.tsx
@@ -0,0 +1,22 @@
+
+const Skills = () => {
+  return (
+    <section className="h-screen w-screen bg-[#293744] text-[#a89fa3] text-center snap-center">
+      <h2 className='text-4xl py-8'>Skills</h2>
+      <div className="py-8 grid grid-flow-col grid-rows-5 gap-4"> {/* Array of mapped icons */}
+        <p>Skill_1</p>
+        <p>Skill_2</p>
+        <p>Skill_3</p>
+        <p>Skill_4</p>
+        <p>Skill_5</p>
+        <p>Skill_6</p>
+        <p>Skill_7</p>
+        <p>Skill_8</p>
+        <p>Skill_9</p>
+        <p>Skill_10</p>
+      </div>
+    </section>
+  )
+}
+
+export default Skills
