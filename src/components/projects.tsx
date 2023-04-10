831b95e89071fe4f7cfb05d054194c8067b94bdc
Anthony Shellman <atshellman@gmail.com>
2023-04-10 17:54:11 -0600

    Initial commit

---
+++ src/components/projects.tsx
@@ -0,0 +1,44 @@
+
+const Projects = () => {
+  return (
+    <section className="h-screen w-screen bg-[#293744] text-[#a89fa3] text-center snap-center">
+      <h2 className='text-4xl py-8'>Projects</h2> {/* Array of mapped objects, swipeable */}
+      <div className="flex justify-evenly">
+        <div>
+          <h4>Project 1 Title</h4>
+          <p>Description</p>
+          <p>Tech Stack</p>
+          <p>Image</p>
+          <p>Deploy Link</p>
+          <p>GitHub Repo</p>
+        </div>
+        <div>
+          <h4>Project 2 Title</h4>
+          <p>Description</p>
+          <p>Tech Stack</p>
+          <p>Image</p>
+          <p>Deploy Link</p>
+          <p>GitHub Repo</p>
+        </div>
+        <div>
+          <h4>Project 3 Title</h4>
+          <p>Description</p>
+          <p>Tech Stack</p>
+          <p>Image</p>
+          <p>Deploy Link</p>
+          <p>GitHub Repo</p>
+        </div>
+        <div>
+          <h4>Project 4 Title</h4>
+          <p>Description</p>
+          <p>Tech Stack</p>
+          <p>Image</p>
+          <p>Deploy Link</p>
+          <p>GitHub Repo</p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Projects
