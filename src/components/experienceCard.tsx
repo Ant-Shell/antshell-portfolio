f499a1cb225531b5dfd3053efdb56eb558934020
Anthony Shellman <atshellman@gmail.com>
2023-04-14 15:30:25 -0600

    Initial commit

---
+++ src/components/experienceCard.tsx
@@ -0,0 +1,34 @@
+import { FunctionComponent } from "react"
+
+type Props = {
+  id: number
+  name: string
+  location: string
+  duration: string
+  overview: Array<string>
+}
+
+const ExperienceCard: FunctionComponent<Props> = ({name, location, duration, overview}) => {
+  const overViewList = overview.map((item, index) => {
+
+    return (
+      <li key={index}>
+      {item}
+      </li>
+    )
+  })
+
+  return (
+    <section>
+      <h3>{name}</h3>
+      <p>{location}</p>
+      <p>{duration}</p>
+      <p>Overview:</p>
+      <ul>
+        {overViewList}
+      </ul>
+    </section>
+  )
+}
+
+export default ExperienceCard
