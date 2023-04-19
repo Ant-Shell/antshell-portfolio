f85292a44c44e6e0ba56e0995d3c1a99c6b342d8
Anthony Shellman <atshellman@gmail.com>
2023-04-18 18:46:20 -0600

    Import and render tech icons

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,19 +1,24 @@
+import { IoLogoCss3 } from "react-icons/io"
+import { IoLogoReact } from 'react-icons/io5'
+import { SiChai, SiGit, SiGithub, SiJavascript, SiMocha,
+        SiPostgresql, SiTailwindcss } from "react-icons/si";
+import { TbBrandCypress } from "react-icons/tb";
 
 const Skills = () => {
   return (
     <section id="skills" className="h-[55rem] w-screen bg-[#293744] text-[#a89fa3] text-center snap-center">
       <h2 className='text-4xl py-8'>Skills</h2>
-      <div className="py-8 grid grid-flow-col grid-rows-5 gap-4"> {/* Array of mapped icons */}
-        <p>Skill_1</p>
-        <p>Skill_2</p>
-        <p>Skill_3</p>
-        <p>Skill_4</p>
-        <p>Skill_5</p>
-        <p>Skill_6</p>
-        <p>Skill_7</p>
-        <p>Skill_8</p>
-        <p>Skill_9</p>
-        <p>Skill_10</p>
+      <div className="py-8 grid grid-flow-col grid-rows-5 gap-4 justify-evenly">
+        <IoLogoReact />
+        <IoLogoCss3 />
+        <SiMocha />
+        <TbBrandCypress />
+        <SiGithub />
+        <SiJavascript />
+        <SiTailwindcss />
+        <SiChai />
+        <SiGit />
+        <SiPostgresql />
       </div>
     </section>
   )
