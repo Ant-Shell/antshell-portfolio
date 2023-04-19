7949ef753934fccd85146665b1b9aae33026d073
Anthony Shellman <atshellman@gmail.com>
2023-04-19 13:32:47 -0600

    Map and render temporary projects

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,37 +1,40 @@
+import useSWR from 'swr'
+import fetcher from '@utilities/apiCalls'
+import ProjectsCard from './projectsCard'
+
+type ProjectData = {
+  id: number
+  name: string
+  description: string
+  image: string
+  deployLink: string
+  githubRepo: string
+}[]
 
 const Projects = () => {
+  const { data, error, isLoading } = useSWR<ProjectData>('/api/projectInfo', fetcher)
+
+  const projectList = data?.map(project => {
+    const {id, name, description, image, deployLink, githubRepo} = project
+
+    return (
+      <ProjectsCard
+      id={id}
+      name={name}
+      description={description}
+      image={image}
+      deployLink={deployLink}
+      githubRepo={githubRepo}
+      key={id}
+      />
+    )
+  })
+
   return (
     <section id="projects" className="h-[55rem] w-screen bg-[#293744] text-[#a89fa3] text-center snap-center">
       <h2 className='text-4xl py-8'>Projects</h2> {/* Array of mapped objects, swipeable */}
       <div className="flex justify-evenly">
-        <div>
-          <h4>Project 1 Title</h4>
-          <p>Description</p>
-          <p>Image</p>
-          <p>Deploy Link</p>
-          <p>GitHub Repo</p>
-        </div>
-        <div>
-          <h4>Project 2 Title</h4>
-          <p>Description</p>
-          <p>Image</p>
-          <p>Deploy Link</p>
-          <p>GitHub Repo</p>
-        </div>
-        <div>
-          <h4>Project 3 Title</h4>
-          <p>Description</p>
-          <p>Image</p>
-          <p>Deploy Link</p>
-          <p>GitHub Repo</p>
-        </div>
-        <div>
-          <h4>Project 4 Title</h4>
-          <p>Description</p>
-          <p>Image</p>
-          <p>Deploy Link</p>
-          <p>GitHub Repo</p>
-        </div>
+        {projectList}
       </div>
     </section>
   )
