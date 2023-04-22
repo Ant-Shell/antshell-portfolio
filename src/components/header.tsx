58b15e7fb2046ac0482edd389f9e4403cb8f9e29
Anthony Shellman <atshellman@gmail.com>
2023-04-22 13:23:17 -0600

    Add functionality for toggling hamburger menu options

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,20 +1,30 @@
 import { GiHamburgerMenu } from 'react-icons/gi'
+import React, {useState} from 'react'
 
 import Link from 'next/link'
 const Header = () => {
+  const [menuVisible, setMenuVisible] = useState<boolean>(false)
+
+  const toggleMenu = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.preventDefault()
+    setMenuVisible(!menuVisible)
+  }
+
   return (
     <>
     <header className="md:hidden h-28 w-screen bg-[#293744] text-[#a89fa3] py-8 text-center snap-start">
-      <div className='flex justify-start px-8'>
+      <div className='flex justify-start px-8' onClick={toggleMenu}>
       {<GiHamburgerMenu className='w-10 h-10'/>}
       </div>
-      <div className='p-5 flex justify-evenly'>
+      {menuVisible ? <div className='p-5 flex justify-evenly'>
         <Link href="#about">About</Link>
         <Link href="#experience">Experience</Link>
         <Link href="#skills">Skills</Link>
         <Link href="#projects">Projects</Link>
         <Link href="#contact">Contact</Link>
       </div>
+      :
+      null}
     </header>
 
     <header className="hidden md:block h-28 w-screen bg-[#293744] text-[#a89fa3] py-8 text-center snap-start">
