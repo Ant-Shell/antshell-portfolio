479e42d5168bb4b7d522bd23947e69fdb58ce9f6
Anthony Shellman <atshellman@gmail.com>
2023-04-22 12:46:58 -0600

    Add hamburger menu icon and link contents

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,23 @@
+import { GiHamburgerMenu } from 'react-icons/gi'
+
 import Link from 'next/link'
 const Header = () => {
   return (
-    <header className="h-28 w-screen bg-[#293744] text-[#a89fa3] py-8 text-center snap-start">
+    <>
+    <header className="md:hidden h-28 w-screen bg-[#293744] text-[#a89fa3] py-8 text-center snap-start">
+      <div className='flex justify-start px-8'>
+      {<GiHamburgerMenu className='w-10 h-10'/>}
+      </div>
+      <div className='p-5 flex justify-evenly'>
+        <Link href="#about">About</Link>
+        <Link href="#experience">Experience</Link>
+        <Link href="#skills">Skills</Link>
+        <Link href="#projects">Projects</Link>
+        <Link href="#contact">Contact</Link>
+      </div>
+    </header>
+
+    <header className="hidden md:block h-28 w-screen bg-[#293744] text-[#a89fa3] py-8 text-center snap-start">
       <div className="flex justify-evenly">
         <Link href="#about">About</Link>
         <Link href="#experience">Experience</Link>
@@ -10,6 +26,7 @@ const Header = () => {
         <Link href="#contact">Contact</Link>
       </div>
     </header>
+    </>
   )
 }
 
