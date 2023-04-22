1656c332fbfaba305ec3a57900e7771b68f1676d
Anthony Shellman <atshellman@gmail.com>
2023-04-22 15:03:37 -0600

    Add bg hover effect to navigation options

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -17,11 +17,11 @@ const Header = () => {
       {<GiHamburgerMenu className='w-10 h-10'/>}
       </div>
       {menuOptionVisibility ? <div className='p-5 flex justify-evenly'>
-        <Link href="#about">About</Link>
-        <Link href="#experience">Experience</Link>
-        <Link href="#skills">Skills</Link>
-        <Link href="#projects">Projects</Link>
-        <Link href="#contact">Contact</Link>
+        <Link href="#about" className='hover:bg-[#535561]'>About</Link>
+        <Link href="#experience" className='hover:bg-[#535561]'>Experience</Link>
+        <Link href="#skills" className='hover:bg-[#535561]'>Skills</Link>
+        <Link href="#projects" className='hover:bg-[#535561]'>Projects</Link>
+        <Link href="#contact" className='hover:bg-[#535561]'>Contact</Link>
       </div>
       :
       null}
@@ -29,11 +29,11 @@ const Header = () => {
 
     <header className="hidden md:block h-28 w-screen bg-[#293744] text-[#a89fa3] py-8 text-center snap-start">
       <div className="flex justify-evenly">
-        <Link href="#about">About</Link>
-        <Link href="#experience">Experience</Link>
-        <Link href="#skills">Skills</Link>
-        <Link href="#projects">Projects</Link>
-        <Link href="#contact">Contact</Link>
+        <Link href="#about" className='hover:bg-[#535561]'>About</Link>
+        <Link href="#experience" className='hover:bg-[#535561]'>Experience</Link>
+        <Link href="#skills" className='hover:bg-[#535561]'>Skills</Link>
+        <Link href="#projects" className='hover:bg-[#535561]'>Projects</Link>
+        <Link href="#contact" className='hover:bg-[#535561]'>Contact</Link>
       </div>
     </header>
     </>
