614dd73b596eac810451314696f2b2053a5f6b1e
Anthony Shellman <atshellman@gmail.com>
2023-04-28 12:52:29 -0600

    Update data, remove placeholder data

--- src/pages/api/experienceInfo.tsx
+++ src/pages/api/experienceInfo.tsx
@@ -13,31 +13,44 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json([
-    {
-      id: 1,
-      name: "HeyAJob",
-      role: "Worker",
-      location: "Someplace, CO",
-      duration: "Feb 2022 - Feb 2023",
-      overview: ["I did a thing", "I did another thing", "Yay, another thing!"]
-    },
-    {
-      id: 2,
-      name: "OKJob",
-      role: "Worker",
-      location: "Overthere, CO",
-      duration: "Feb 2021 - Feb 2022",
-      overview: ["I did a thing", "I did another thing", "Yay, another thing!"]
-    },
-    {
-      id: 3,
-      name: "Job, Inc.",
-      role: "Worker",
-      location: "Overhere, CO",
-      duration: "Feb 2020 - Feb 2021",
-      overview: ["I did a thing", "I did another thing", "Yay, another thing!"]
-    },
-  ]
+  res.status(200).json(
+    [
+      {
+        id: 1,
+        name: "Chainlink Labs",
+        role: "Network and Systems Administrator",
+        location: "Remote",
+        duration: "May 2021 - Aug 2021",
+        overview: [
+          "- Managed and maintained decentralized blockchain node applications in cloud environment",
+          "- Ensured 24/7 performance of nodes through shift-based on-call rotation",
+          "- Utilized AWS, Docker, Grafana and Prometheus for deployments and monitoring"
+        ]
+      },
+      {
+        id: 2,
+        name: "Gensler",
+        role: "Server Administrator",
+        location: "Denver, CO",
+        duration: "Feb 2019 - May 2021",
+        overview: [
+          "- Administered server life-cycles across multiple co-locations",
+          "- Improved existing technical infrastructure",
+          "- Partnered with development team to successfully launch code releases"
+        ]
+      },
+      {
+        id: 3,
+        name: "HomeAdvisor",
+        role: "Systems Administrator",
+        location: "Golder, CO",
+        duration: "Feb 2012 - Feb 2019",
+        overview: [
+          "- Ensured infrastructure remained secure and up to date through regular maintenance",
+          "- Investigated, evaluated and deployed solutions to meet business needs",
+          "- Resolved system issues quickly and efficiently"
+        ]
+      },
+    ]
   )
 }
