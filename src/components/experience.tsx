6b23589f6ea3839af89cef92bb83d57acd3b5b91
Anthony Shellman <atshellman@gmail.com>
2023-04-28 11:17:22 -0600

    Add role property, adjust section height

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -5,6 +5,7 @@ import ExperienceCard from './experienceCard'
 type ExperienceData = {
   id: number
   name: string
+  role: string
   location: string
   duration: string
   overview: Array<string>
@@ -14,12 +15,13 @@ const Experience = () => {
   const { data, error, isLoading } = useSWR<ExperienceData>('/api/experienceInfo', fetcher)
 
   const experienceList = data?.map(experience => {
-    const {id, name, location, duration, overview} = experience
+    const {id, name, role, location, duration, overview} = experience
 
     return (
       <ExperienceCard
       id={id}
       name={name}
+      role={role}
       location={location}
       duration={duration}
       overview={overview}
@@ -29,7 +31,7 @@ const Experience = () => {
   })
 
   return (
-    <section id="experience" className="h-[55rem] w-screen bg-[#293744] text-[#a89fa3] text-center snap-center">
+    <section id="experience" className="h-[35rem] w-screen bg-[#293744] text-[#a89fa3] text-center snap-center">
       <div className="py-8">
         <h2 className='text-3xl md:text-4xl lg:text-5xl xl:text-6xl py-8'>Experience</h2>
         <div className="py-8 flex justify-around">
