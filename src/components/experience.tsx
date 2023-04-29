03a4c62532beb4da8810fd5e6e8e2560c09f8d00
Anthony Shellman <atshellman@gmail.com>
2023-04-29 16:23:37 -0600

    Add type, update logo param, add state, map logos, add handleClick and singleExperience functions

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -1,5 +1,8 @@
+import { StaticImageData } from 'next/image'
+import React, {useState} from 'react'
 import useSWR from 'swr'
 import fetcher from '@utilities/apiCalls'
+import Image from 'next/image'
 import ExperienceCard from './experienceCard'
 
 type ExperienceData = {
@@ -8,34 +11,72 @@ type ExperienceData = {
   role: string
   location: string
   duration: string
+  logo: StaticImageData
   overview: Array<string>
 }[]
 
+type SingleExperienceDetails = {
+  id: number
+  name: string
+  role: string
+  location: string
+  duration: string
+  logo: StaticImageData
+  overview: Array<string>
+}
+
 const Experience = () => {
   const { data, error, isLoading } = useSWR<ExperienceData>('/api/experienceInfo', fetcher)
+  const [description, setDescription] = useState<SingleExperienceDetails | undefined>(undefined)
 
-  const experienceList = data?.map(experience => {
-    const {id, name, role, location, duration, overview} = experience
+  const handleClick = (e:React.MouseEvent<HTMLDivElement, MouseEvent>, id:number):void => {
+    e.preventDefault()
+    const expinfo = data?.find(element => element.id === id)
+    setDescription(expinfo)
+  }
+
+  const logoList = data?.map(experience => {
+    const { id, name, logo } = experience
+
+    return (
+      <div key={id} className='hover:cursor-pointer' onClick={(e)=> handleClick(e, id)}>
+        <Image
+        src={logo}
+        alt={name + "logo"}
+        className='h-20 w-20 md:h-40 md:w-40
+          lg:h-60 lg:w-60 xl:h-80 xl:w-80'
+        />
+      </div>
+    )
+  })
+
+  const singleExperience = (description: SingleExperienceDetails) => {
+    const {id, name, role, location, duration, logo, overview} = description
 
     return (
       <ExperienceCard
-      id={id}
-      name={name}
-      role={role}
-      location={location}
-      duration={duration}
-      overview={overview}
-      key={id}
+        id={id}
+        name={name}
+        role={role}
+        location={location}
+        duration={duration}
+        logo={logo}
+        overview={overview}
+        key={id}
       />
     )
-  })
+  }
 
   return (
     <section id="experience" className="h-[35rem] w-screen bg-[#293744] text-[#a89fa3] text-center snap-center">
       <div className="py-8">
         <h2 className='text-3xl md:text-4xl lg:text-5xl xl:text-6xl py-8'>Experience</h2>
         <div className="py-8 flex justify-around">
-          {experienceList}
+          {description === undefined ?
+            logoList
+            :
+            singleExperience(description)
+          }
         </div>
       </div>
     </section>
