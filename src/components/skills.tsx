c7a46b0967f2827ac17d779eb62d545d16c50992
Anthony Shellman <atshellman@gmail.com>
2023-04-30 19:55:28 -0600

    Adjust grid responsiveness, add and render new icons

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,24 +1,38 @@
-import { IoLogoCss3 } from "react-icons/io"
+import { IoLogoCss3, IoLogoNodejs } from "react-icons/io"
 import { IoLogoReact } from 'react-icons/io5'
 import { SiChai, SiGit, SiGithub, SiJavascript, SiMocha,
-        SiPostgresql, SiTailwindcss } from "react-icons/si";
+        SiPostgresql, SiTailwindcss, SiTypescript } from "react-icons/si";
 import { TbBrandCypress } from "react-icons/tb";
 
 const Skills = () => {
   return (
     <section id="skills" className="h-[55rem] w-screen bg-[#293744] text-[#a89fa3] text-center snap-center">
       <h2 className='text-3xl md:text-4xl lg:text-5xl xl:text-6xl py-8'>Skills</h2>
-      <div className="py-8 grid grid-flow-col grid-rows-5 gap-4 justify-evenly">
-        <IoLogoReact />
-        <IoLogoCss3 />
-        <SiMocha />
-        <TbBrandCypress />
-        <SiGithub />
-        <SiJavascript />
-        <SiTailwindcss />
-        <SiChai />
-        <SiGit />
-        <SiPostgresql />
+      <div className="p-8 grid grid-rows-4 grid-cols-3 gap-4 justify-items-center">
+        <IoLogoReact className="h-24 w-24 md:h-28 md:w-28
+          lg:h-32 lg:w-32 xl:h-36 xl:w-36" />
+        <SiJavascript className="h-24 w-24 md:h-28 md:w-28
+        lg:h-32 lg:w-32 xl:h-36 xl:w-36"/>
+        <SiTypescript className="h-24 w-24 md:h-28 md:w-28
+          lg:h-32 lg:w-32 xl:h-36 xl:w-36"/>
+        <IoLogoCss3 className="h-24 w-24 md:h-28 md:w-28
+          lg:h-32 lg:w-32 xl:h-36 xl:w-36" />
+        <SiTailwindcss className="h-24 w-24 md:h-28 md:w-28
+        lg:h-32 lg:w-32 xl:h-36 xl:w-36"/>
+        <IoLogoNodejs className="h-24 w-24 md:h-28 md:w-28
+          lg:h-32 lg:w-32 xl:h-36 xl:w-36"/>
+        <SiMocha className="h-24 w-24 md:h-28 md:w-28
+          lg:h-32 lg:w-32 xl:h-36 xl:w-36" />
+        <SiChai className="h-24 w-24 md:h-28 md:w-28
+          lg:h-32 lg:w-32 xl:h-36 xl:w-36"/>
+        <TbBrandCypress className="h-24 w-24 md:h-28 md:w-28
+          lg:h-32 lg:w-32 xl:h-36 xl:w-36" />
+        <SiGithub className="h-24 w-24 md:h-28 md:w-28
+          lg:h-32 lg:w-32 xl:h-36 xl:w-36"/>
+        <SiGit className="h-24 w-24 md:h-28 md:w-28
+          lg:h-32 lg:w-32 xl:h-36 xl:w-36"/>
+        <SiPostgresql className="h-24 w-24 md:h-28 md:w-28
+          lg:h-32 lg:w-32 xl:h-36 xl:w-36"/>
       </div>
     </section>
   )
