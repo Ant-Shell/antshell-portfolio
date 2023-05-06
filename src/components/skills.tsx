9d5f7c89869937e78b77701a8c13c4465d14f1c4
Anthony Shellman <atshellman@gmail.com>
2023-05-06 11:17:15 -0600

    Slight styling refactor for readability

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -5,34 +5,25 @@ import { SiChai, SiGit, SiGithub, SiJavascript, SiMocha,
 import { TbBrandCypress } from "react-icons/tb";
 
 const Skills = () => {
+
+  const logoStyles = "h-24 w-24 md:h-28 md:w-28 lg:h-32 lg:w-32 xl:h-36 xl:w-36"
+
   return (
     <section id="skills" className="h-[55rem] w-screen bg-[#293744] text-[#a89fa3] text-center snap-center">
       <h2 className='text-3xl md:text-4xl lg:text-5xl xl:text-6xl py-8'>Skills</h2>
       <div className="p-8 grid grid-rows-4 grid-cols-3 gap-4 justify-items-center">
-        <IoLogoReact className="h-24 w-24 md:h-28 md:w-28
-          lg:h-32 lg:w-32 xl:h-36 xl:w-36" />
-        <SiJavascript className="h-24 w-24 md:h-28 md:w-28
-        lg:h-32 lg:w-32 xl:h-36 xl:w-36"/>
-        <SiTypescript className="h-24 w-24 md:h-28 md:w-28
-          lg:h-32 lg:w-32 xl:h-36 xl:w-36"/>
-        <IoLogoCss3 className="h-24 w-24 md:h-28 md:w-28
-          lg:h-32 lg:w-32 xl:h-36 xl:w-36" />
-        <SiTailwindcss className="h-24 w-24 md:h-28 md:w-28
-        lg:h-32 lg:w-32 xl:h-36 xl:w-36"/>
-        <IoLogoNodejs className="h-24 w-24 md:h-28 md:w-28
-          lg:h-32 lg:w-32 xl:h-36 xl:w-36"/>
-        <SiMocha className="h-24 w-24 md:h-28 md:w-28
-          lg:h-32 lg:w-32 xl:h-36 xl:w-36" />
-        <SiChai className="h-24 w-24 md:h-28 md:w-28
-          lg:h-32 lg:w-32 xl:h-36 xl:w-36"/>
-        <TbBrandCypress className="h-24 w-24 md:h-28 md:w-28
-          lg:h-32 lg:w-32 xl:h-36 xl:w-36" />
-        <SiGithub className="h-24 w-24 md:h-28 md:w-28
-          lg:h-32 lg:w-32 xl:h-36 xl:w-36"/>
-        <SiGit className="h-24 w-24 md:h-28 md:w-28
-          lg:h-32 lg:w-32 xl:h-36 xl:w-36"/>
-        <SiPostgresql className="h-24 w-24 md:h-28 md:w-28
-          lg:h-32 lg:w-32 xl:h-36 xl:w-36"/>
+        <IoLogoReact className={logoStyles} />
+        <SiJavascript className={logoStyles} />
+        <SiTypescript className={logoStyles} />
+        <IoLogoCss3 className={logoStyles} />
+        <SiTailwindcss className={logoStyles} />
+        <IoLogoNodejs className={logoStyles} />
+        <SiMocha className={logoStyles} />
+        <SiChai className={logoStyles} />
+        <TbBrandCypress className={logoStyles} />
+        <SiGithub className={logoStyles} />
+        <SiGit className={logoStyles} />
+        <SiPostgresql className={logoStyles} />
       </div>
     </section>
   )
