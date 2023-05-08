68e60b281ef055bacb6efb76ff63318ed93fe51f
Anthony Shellman <atshellman@gmail.com>
2023-05-07 19:35:42 -0600

    Adjust styling for responsiveness across various devices

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -37,7 +37,7 @@ const Projects = () => {
   })
 
   return (
-    <section id="projects" className="h-[70rem] w-screen bg-[#293744] text-[#a89fa3] text-center snap-center">
+    <section id="projects" className="h-[30rem] md:h-[45rem] lg:h-[50rem] xl:h-[60rem] w-screen bg-[#293744] text-[#a89fa3] text-center snap-center">
       <h2 className='text-3xl md:text-4xl lg:text-5xl xl:text-6xl py-8'>Projects</h2>
           <Swiper className='h-[17rem] w-[20rem] md:h-[40rem] md:w-[50rem] lg:h-[40rem] lg:w-[60rem] xl:h-[50rem] xl:w-[70rem]'
             modules={[Navigation, Keyboard]}
