6ab4a40ab79180cb9718ddccbecbac6f4bb6aad9
Anthony Shellman <atshellman@gmail.com>
2023-05-09 17:32:40 -0600

    Adjust sizing for responsiveness

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -37,9 +37,9 @@ const Projects = () => {
   })
 
   return (
-    <section id="projects" className="h-[30rem] md:h-[45rem] lg:h-[50rem] xl:h-[60rem] w-screen bg-[#293744] text-[#a89fa3] text-center snap-always snap-start">
+    <section id="projects" className="h-[30rem] md:h-[50rem] lg:h-[55rem] xl:h-[65rem] w-screen bg-[#293744] text-[#a89fa3] text-center snap-always snap-start">
       <h2 className='text-3xl md:text-4xl lg:text-5xl xl:text-6xl py-8'>Projects</h2>
-          <Swiper className='h-[17rem] w-[20rem] md:h-[40rem] md:w-[50rem] lg:h-[40rem] lg:w-[60rem] xl:h-[50rem] xl:w-[70rem]'
+          <Swiper className='h-[25rem] w-[20rem] md:h-[40rem] md:w-[50rem] lg:h-[50rem] lg:w-[60rem] xl:h-[50rem] xl:w-[70rem]'
             modules={[Navigation, Keyboard]}
             navigation={true}
             keyboard={true}
