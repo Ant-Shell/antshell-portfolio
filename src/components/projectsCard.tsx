d43ed1e226b48e423433ddf6a33b7d5f2fcda534
Anthony Shellman <atshellman@gmail.com>
2023-05-09 19:20:14 -0600

    Relocate description

--- src/components/projectsCard.tsx
+++ src/components/projectsCard.tsx
@@ -16,10 +16,10 @@ const ProjectsCard:FC<Props> = ({id, name, description, image, deployLink, githu
   return (
     <section key={id} className="h-fit w-fit">
       <h3 className="text-lg md:text-xl lg:text-2xl xl:text-3xl p-1">{name}</h3>
+      <p className="md:text-lg lg:text-xl xl:text-2xl p-1">{description}</p>
       <a href={deployLink} target="_blank" rel="noopener noreferrer">
         <Image src={image} alt={name + "preview"}/>
       </a>
-      <p className="md:text-lg lg:text-xl xl:text-2xl p-1">{description}</p>
       <p className="md:text-lg lg:text-xl xl:text-2xl p-1"><a href={deployLink} target="_blank" rel="noopener noreferrer">Click here to check it out!</a></p>
       <p className="md:text-lg lg:text-xl xl:text-2xl p-1"><a href={githubRepo} target="_blank" rel="noopener noreferrer">Documentation can be found here</a></p>
     </section>
