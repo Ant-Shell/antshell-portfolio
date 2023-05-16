c250f8f77a105681462b858131e1d4a7fd02a63f
Anthony Shellman <atshellman@gmail.com>
2023-05-15 20:02:07 -0600

    Add logo colors

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -12,18 +12,18 @@ const Skills = () => {
     <section id="skills" className="h-[40rem] md:h-[45rem] lg:h-[50rem] xl:h-[55rem]">
       <h2 className='text-3xl md:text-4xl lg:text-5xl xl:text-6xl py-8'>Skills</h2>
       <div className="p-8 grid grid-rows-4 grid-cols-3 gap-4 justify-items-center">
-        <IoLogoReact className={logoStyles} />
-        <SiJavascript className={logoStyles} />
-        <SiTypescript className={logoStyles} />
-        <IoLogoCss3 className={logoStyles} />
-        <SiTailwindcss className={logoStyles} />
-        <IoLogoNodejs className={logoStyles} />
-        <SiMocha className={logoStyles} />
-        <SiChai className={logoStyles} />
-        <TbBrandCypress className={logoStyles} />
-        <SiGithub className={logoStyles} />
-        <SiGit className={logoStyles} />
-        <SiPostgresql className={logoStyles} />
+        <IoLogoReact className={`${logoStyles} text-[#479dc6]`} />
+        <SiJavascript className={`${logoStyles} text-[#e1d063]`} />
+        <SiTypescript className={`${logoStyles} text-[#4278c0]`} />
+        <IoLogoCss3 className={`${logoStyles} text-[#2b53dc]`} />
+        <SiTailwindcss className={`${logoStyles} text-[#60Bcf2]`} />
+        <IoLogoNodejs className={`${logoStyles} text-[#5c8256]`} />
+        <SiMocha className={`${logoStyles} text-[#83654b]`} />
+        <SiChai className={`${logoStyles} text-[#931f14]`} />
+        <TbBrandCypress className={`${logoStyles} text-[#83CFaa]`} />
+        <SiGithub className={`${logoStyles} text-[#000]`} />
+        <SiGit className={`${logoStyles} text-[#e35938]`} />
+        <SiPostgresql className={`${logoStyles} text-[#3f678d]`} />
       </div>
     </section>
   )
