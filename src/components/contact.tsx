27bfbe3187d25494fd2df40126fe7c03d77720b1
Anthony Shellman <atshellman@gmail.com>
2023-05-17 15:16:46 -0600

    Add Aria labels for accessibility

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -9,13 +9,16 @@ const Contact = () => {
     <section id="contact" className="h-72 md:h-80">
       <h2 className='text-3xl md:text-4xl lg:text-5xl xl:text-6xl py-8'>Contact Me</h2>
       <div className='flex justify-around'>
-      <a href='https://www.linkedin.com/in/anthonyshellman/' target="_blank" rel="noopener noreferrer">
+      <a href='https://www.linkedin.com/in/anthonyshellman/' target="_blank" rel="noopener noreferrer" 
+        aria-label="Link to anthonyshellman LinkedIn profile">
         <SiLinkedin className={`${logoStyles} text-[#2967bc]`} />
       </a>
-      <a href='https://github.com/Ant-Shell' target="_blank" rel="noopener noreferrer">
+      <a href='https://github.com/Ant-Shell' target="_blank" rel="noopener noreferrer" role="button"
+        aria-label="Link to Ant-Shell GitHub profile">
         <VscGithub className={`${logoStyles} text-[#000]`} />
       </a>
-      <a href="mailto:atshellman@gmail.com">
+      <a href="mailto:atshellman@gmail.com" role="button" 
+        aria-label="Link to email atshellman@gmail.com">
         <SiGmail className={`${logoStyles} text-[#da5040]`} />
       </a>
       </div>
