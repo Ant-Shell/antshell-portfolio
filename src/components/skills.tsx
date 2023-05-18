c09e3d8bde0ccdbe70bdefae43be349e3ab463e9
Anthony Shellman <atshellman@gmail.com>
2023-05-18 13:40:33 -0600

    Add links for skills, ARIA labels

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -12,18 +12,54 @@ const Skills = () => {
     <section id="skills" className="h-[40rem] md:h-[45rem] lg:h-[50rem] xl:h-[55rem]">
       <h2 className='text-3xl md:text-4xl lg:text-5xl xl:text-6xl py-8'>Skills</h2>
       <div className="p-8 grid grid-rows-4 grid-cols-3 gap-4 justify-items-center">
-        <IoLogoReact className={`${logoStyles} text-[#479dc6]`} />
-        <SiJavascript className={`${logoStyles} text-[#e1d063]`} />
-        <SiTypescript className={`${logoStyles} text-[#4278c0]`} />
-        <IoLogoCss3 className={`${logoStyles} text-[#2b53dc]`} />
-        <SiTailwindcss className={`${logoStyles} text-[#60Bcf2]`} />
-        <IoLogoNodejs className={`${logoStyles} text-[#5c8256]`} />
-        <SiMocha className={`${logoStyles} text-[#83654b]`} />
-        <SiChai className={`${logoStyles} text-[#931f14]`} />
-        <TbBrandCypress className={`${logoStyles} text-[#83CFaa]`} />
-        <SiGithub className={`${logoStyles} text-[#000]`} />
-        <SiGit className={`${logoStyles} text-[#e35938]`} />
-        <SiPostgresql className={`${logoStyles} text-[#3f678d]`} />
+        <a href='https://react.dev/' target="_blank" rel="noopener noreferrer"
+        aria-label="Link to react.dev">
+          <IoLogoReact className={`${logoStyles} text-[#479dc6]`} />
+        </a>
+        <a href='https://www.javascript.com/' target="_blank" rel="noopener noreferrer"
+        aria-label="Link to javascript.com">
+          <SiJavascript className={`${logoStyles} text-[#e1d063]`} />
+        </a>
+        <a href='https://www.typescriptlang.org/' target="_blank" rel="noopener noreferrer"
+        aria-label="Link to typescriptlang.org">
+          <SiTypescript className={`${logoStyles} text-[#4278c0]`} />
+        </a>
+        <a href='https://www.css3.com/' target="_blank" rel="noopener noreferrer"
+        aria-label="Link to css3.com">
+          <IoLogoCss3 className={`${logoStyles} text-[#2b53dc]`} />
+        </a>
+        <a href='https://tailwindcss.com/' target="_blank" rel="noopener noreferrer"
+        aria-label="Link to tailwindcss.com">
+          <SiTailwindcss className={`${logoStyles} text-[#60Bcf2]`} />
+        </a>
+        <a href='https://nodejs.org/' target="_blank" rel="noopener noreferrer"
+        aria-label="Link to nodejs.org">
+          <IoLogoNodejs className={`${logoStyles} text-[#5c8256]`} />
+        </a>
+        <a href='https://mochajs.org/' target="_blank" rel="noopener noreferrer"
+        aria-label="Link to mochajs.org">
+          <SiMocha className={`${logoStyles} text-[#83654b]`} />
+        </a>
+        <a href='https://www.chaijs.com/' target="_blank" rel="noopener noreferrer"
+        aria-label="Link to chaijs.com">
+          <SiChai className={`${logoStyles} text-[#931f14]`} />
+        </a>
+        <a href='https://www.cypress.io/' target="_blank" rel="noopener noreferrer"
+        aria-label="Link to cypress.io">
+          <TbBrandCypress className={`${logoStyles} text-[#83CFaa]`} />
+        </a>
+        <a href='https://github.com/' target="_blank" rel="noopener noreferrer"
+        aria-label="Link to github.com">
+          <SiGithub className={`${logoStyles} text-[#000]`} />
+        </a>
+        <a href='https://git-scm.com/' target="_blank" rel="noopener noreferrer"
+        aria-label="Link to git-scm.com">
+          <SiGit className={`${logoStyles} text-[#e35938]`} />
+        </a>
+        <a href='https://www.postgresql.org/' target="_blank" rel="noopener noreferrer"
+        aria-label="Link to postgresql.org">
+          <SiPostgresql className={`${logoStyles} text-[#3f678d]`} />
+        </a>  
       </div>
     </section>
   )
